be0a7c4f9d684d78c57797a16c3a47b2fc255145
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-02 23:22:59 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,19 +1,29 @@
- <span
+<span
+  style={{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    userSelect: 'none',
+  }}
+  onClick={e => {
+    e.stopPropagation();
 
+    if (original.id != null) {
+      toggleParent(original.id);
+    }
+  }}
+>
+  <span
     style={{
-
-      display: 'inline-block',
-
+      display: 'inline-flex',
       transform: original._isOpen
-
         ? 'rotate(0deg)'
-
         : 'rotate(-90deg)',
-
     }}
-
   >
-
     <Icon slug="chevron-down" />
-
   </span>
+</span>
