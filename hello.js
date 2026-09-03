2b5c86c5d141d60ed55199dd42e1cd6ce20248de
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-03 17:23:44 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,21 +1,4 @@
-openssl s_client \
-  -connect hvault.staging.echonet:443 \
-  -servername hvault.staging.echonet \
-  -CAfile ~/.certs/keychain.bundle.20231124.pem \
-  </dev/null 2>&1 | grep -E "Verify return code|verify error|subject=|issuer="
-
-
-python -c "import os; print('REQUESTS_CA_BUNDLE=', os.getenv('REQUESTS_CA_BUNDLE')); print('SSL_CERT_FILE=', os.getenv('SSL_CERT_FILE'))"
-
-{
-  "name": "Python Debugger: Flask",
-  "type": "debugpy",
-  "request": "launch",
-  "module": "flask",
-
-  "env": {
-    "REQUESTS_CA_BUNDLE": "${env:HOME}/.certs/keychain.bundle.20231124.pem",
-    "SSL_CERT_FILE": "${env:HOME}/.certs/keychain.bundle.20231124.pem",
-    "PIP_CERT": "${env:HOME}/.certs/keychain.bundle.20231124.pem"
-  }
-}
+git stash push -m "other changes from feat/270114" -- \
+src/pages/InitiativePipelineCard/components/InitiativeDataCard/InitiativeDataCard.js \
+src/pages/InitiativePipelineCard/components/InitiativeDataForm/InitiativeDataForm.js \
+src/pages/InitiativePipelineCard/components/InitiativePipelineTasksTab/columns.js
