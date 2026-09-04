b45b3dc7c24ad6fda3f7ffb124d66a68f8efe72a
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-04 13:36:07 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,48 @@
-git stash push -m "other changes from feat/270114" -- \
-src/pages/InitiativePipelineCard/components/InitiativeDataCard/InitiativeDataCard.js \
-src/pages/InitiativePipelineCard/components/InitiativeDataForm/InitiativeDataForm.js \
-src/pages/InitiativePipelineCard/components/InitiativePipelineTasksTab/columns.js
+<SubSection
+  title={
+    <FormattedMessage
+      id="INITIATIVE_DATA.SUBSECTION.BUSINESS"
+      defaultMessage="Business"
+    />
+  }
+>
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 16,
+    }}
+  >
+    <SmartField
+      isEditing={isEditing}
+      intl={intl}
+      name="aumExpected"
+      labelId="INITIATIVE_DATA.AUM_EXPECTED"
+      component={CommonStyle.Input}
+      data={initiative}
+      displaySnakeKey="aum_expected"
+    />
+
+    <SmartField
+      isEditing={isEditing}
+      intl={intl}
+      name="commentsBusiness"
+      labelId="INITIATIVE_DATA.COMMENTS_BUSINESS"
+      component={TextArea}
+      fieldProps={LongTextProps}
+      data={initiative}
+      displaySnakeKey="commentsBusiness"
+    />
+  </div>
+
+  <SmartField
+    isEditing={isEditing}
+    intl={intl}
+    name="feesDesc"
+    labelId="INITIATIVE_DATA.FEES_DESC"
+    component={TextArea}
+    fieldProps={LongTextProps}
+    data={initiative}
+    displaySnakeKey="fees_desc"
+  />
+</SubSection>
