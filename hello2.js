8845eafd374e4aa4d65f586b9a3ed2c9798486db
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-08 10:53:49 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,20 +1,15 @@
-<NewTextArea
-  value={inlineDraft.description || ""}
-  onChange={e =>
-    updateInlineDraft(
-      original.template_name,
-      "description",
-      e.target.value
-    )
-  }
-  placeholder="Description"
-/>
-
-<NewTextArea
-  value={draft?.description ?? ""}
-  onChange={e =>
-    updateDraft("description", e.target.value)
-  }
-  placeholder="Description"
-/>
-import { NewTextArea } from "@component-studio/ui";
+DescField: styled.textarea`
+  width: ${widthColumns[2] - widthColumns[5]}px;
+  max-width: ${widthColumns[2] - widthColumns[5]}px;
+  min-width: ${widthColumns[2] - widthColumns[5]}px;
+  height: 60px;
+  min-height: 60px;
+  max-height: 90px;
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+  padding: 12px;
+  font-size: 14px;
+  outline: none;
+  box-sizing: border-box;
+  font-family: "BNPP Sans";
+`,
