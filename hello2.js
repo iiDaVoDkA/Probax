979c19fc994b428e74da6afe68de7fcff7955109
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-09 11:25:52 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,15 +1,3 @@
-DescField: styled.textarea`
-  width: ${widthColumns[2] - widthColumns[5]}px;
-  max-width: ${widthColumns[2] - widthColumns[5]}px;
-  min-width: ${widthColumns[2] - widthColumns[5]}px;
-  height: 60px;
-  min-height: 60px;
-  max-height: 90px;
-  border: 1px solid #d9d9d9;
-  border-radius: 4px;
-  padding: 12px;
-  font-size: 14px;
-  outline: none;
-  box-sizing: border-box;
-  font-family: "BNPP Sans";
-`,
+print("RESOURCE reporting =", reporting_services_desc)
+print("RESOURCE custodian =", custodian_services_desc)
+print("RESOURCE other =", other_services_desc)
