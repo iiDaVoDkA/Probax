5112a1f030d237c59ce9ea2e2f2dd16a655a29f7
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-09 11:33:28 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,3 +1,3 @@
-print("RESOURCE reporting =", reporting_services_desc)
-print("RESOURCE custodian =", custodian_services_desc)
-print("RESOURCE other =", other_services_desc)
+print("RESOURCE reporting =", kwargs.get("reporting_services_desc"), flush=True)
+print("RESOURCE custodian =", kwargs.get("custodian_services_desc"), flush=True)
+print("RESOURCE other =", kwargs.get("other_services_desc"), flush=True)
