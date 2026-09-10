93c1913182a592807b1021c394ebe96b90da9eb7
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-10 15:12:37 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,10 +1,15 @@
 if UserRoleEnum.SUPER_ADMIN.value in user.get("roles", []):
     new_project_status = ProjectStatus(status)
 
-    with SessionCriticalActionManager(...):  # mêmes paramètres que le bloc existant plus bas
-        ProjectRepository.update(
-            project=project,
-            status=new_project_status,
-        )
+    ProjectDataChangeRepository.create(
+        project_id=project.id,
+        data_name=ProjectDataNames.PROJECT_STATUS,
+        old_value=project.status.value,
+        new_value=new_project_status.value,
+        user_id=user["id"],
+    )
+
+    project.status = new_project_status
+    project.flush()
 
     return project.to_json(for_project_card=True)
