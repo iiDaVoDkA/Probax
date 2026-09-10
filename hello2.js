d9b94ed516e6003446c1e7ebb51121bc86021a4c
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-10 10:13:21 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,4 +1,11 @@
-def upgrade():
-    op.execute(
-        "ALTER TYPE userroleenum ADD VALUE IF NOT EXISTS 'SUPER_ADMIN'"
-    )
+task = TaskRepository.get(task_id)
+
+if UserRoleEnum.SUPER_ADMIN.value in user.get("roles", []):
+    task = TaskRepository.validate(task, user.get("id"))
+else:
+    if not can_modify_task(user, task):
+        raise PermissionError
+
+    task = validate_task(task, user)
+
+return task.to_json(load_task_details=True)
