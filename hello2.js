b6d36a8e1d45f9c3ae777184ae529951aa42b947
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-11 09:40:30 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,15 +1,1 @@
-if UserRoleEnum.SUPER_ADMIN.value in user.get("roles", []):
-    new_project_status = ProjectStatus(status)
-
-    ProjectDataChangeRepository.create(
-        project_id=project.id,
-        data_name=ProjectDataNames.PROJECT_STATUS,
-        old_value=project.status.value,
-        new_value=new_project_status.value,
-        user_id=user["id"],
-    )
-
-    project.status = new_project_status
-    project.flush()
-
-    return project.to_json(for_project_card=True)
+pytest test/api/project/test_update_project_status.py::TestUpdateProjectStatus::test_cancel_project -vv -s
