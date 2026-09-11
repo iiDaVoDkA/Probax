62ee9e1e0bb8f724581803e2ab8df72892047988
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-11 09:55:19 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,1 +1,2 @@
-pytest --collect-only -vv test/api/project/test_update_project_status.py
+pytest test/api/project/test_update_project_status.py::TestUpdateProjectStatus::test_cancel_project -vv -s
+pytest test/api/project/test_update_project_status.py -vv -s
