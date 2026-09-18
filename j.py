0a18a7ce196286b2c9300b152e97709c0deeb78e
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-18 11:43:18 +0200

    Update j.py

--- j.py
+++ j.py
@@ -1,26 +1,13 @@
-change = (
-    ProjectDataChange.query
-    .order_by(ProjectDataChange.id.desc())
-    .first()
-)
+def get_users_by_role(role):
+    """Calls the PLM API to get users by role"""
+    logging.debug("[PLM] Getting users by role ...")
+    session = get_new_requests_session()
 
-self.assertEqual(change.project_id, 1)
-self.assertEqual(change.data_name, ProjectDataNames.PROJECT_STATUS)
-self.assertEqual(change.old_value, ProjectStatus.ONHOLD.value)
-self.assertEqual(change.new_value, ProjectStatus.COMPLETED.value)
-self.assertEqual(change.user_id, 1)
-self.assertIsNone(change.task_id)
+    url = f"{config.PLM_URL}/user_role?role={role}"
+    headers = {"Authorization": request.headers.get("Authorization")}
 
+    response = session.get(url, headers=headers, verify=False)
+    handle_http_error(response)
 
-change = (
-    ProjectDataChange.query
-    .order_by(ProjectDataChange.id.desc())
-    .first()
-)
-
-self.assertEqual(change.project_id, self.normal_task.project_id)
-self.assertEqual(change.task_id, self.normal_task.id)
-self.assertEqual(change.data_name, ProjectDataNames.TASK_STATUS)
-self.assertEqual(change.old_value, TaskStatus.ONGOING.value)
-self.assertEqual(change.new_value, TaskStatus.DONE.value)
-self.assertEqual(change.user_id, 1)
+    logging.debug("[PLM] users by role loaded.")
+    return response.json()
