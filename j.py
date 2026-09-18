8957389ecfdbaf10308f1fa665ac483a364a2599
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-18 17:14:20 +0200

    Update j.py

--- j.py
+++ j.py
@@ -1,10 +1,17 @@
-@patch(
-    "util.required_plm_authentication._me",
-    return_value=make_fake_me_response(),
-)
-@patch(
-    "clients.oidc.validate_token",
-    return_value={"uid": "mySpecialuid"},
-)
-class TestInitiativeTeamMembers(unittest.TestCase):
-    ...
+deactivate
+
+brew install python@3.12
+
+mv venv venv-py311-backup
+
+$(brew --prefix python@3.12)/bin/python3.12 -m venv venv
+
+source venv/bin/activate
+
+python --version
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+uvicorn app.main:app --reload --port 8000
