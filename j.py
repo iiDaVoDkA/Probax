6808fe0b9b59ba42f3f21d689f011c32d0b3e70e
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-21 13:51:58 +0200

    Update j.py

--- j.py
+++ j.py
@@ -1,28 +1,7 @@
-[INITIATIVE_EDIT_COLUMN]: {
-  Header: <TableHeader label="INITIATIVE_EDIT" sortable={false} />,
-  id: 'initiativeEdit',
-  minWidth: NumberGrid(12),
-  className: 'center',
-
-  Cell: ({ original }: any) => {
-    const canEdit =
-      isAdmin ||
-      original.initiativeOwnerId === user.id;
-
-    if (!canEdit) {
-      return null;
-    }
-
-    return (
-      <IconButton
-        hoverColor={BNPColors.ceruleanBlue}
-        onClick={() => onEditInitiative(original)}
-      >
-        <Icon slug="pen" size={16} />
-      </IconButton>
-    );
-  },
-
-  sortable: false,
-  filterable: false,
-},
+Cell: (props: any) => (
+  <CommitteeDateCell
+    key={`committee-date-${props.original.id}`}
+    original={props.original}
+    value={props.value}
+  />
+),
