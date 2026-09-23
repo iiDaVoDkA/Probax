b9afb2b7bd8c2277e9b56c4f01a6e8b37a7aa6ab
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-23 14:22:31 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,73 +1,11 @@
-const handleEdit = () => {
-  setDateError('');
-  setEditedValue(value || '');
-  setIsEditing(true);
-};
+<span
+  style={{
+    fontSize: 14,
+    lineHeight: '18px',
+    whiteSpace: 'nowrap',
+  }}
+>
+  {value || '-'}
+</span>
 
-const handleCancel = () => {
-  setDateError('');
-  setEditedValue(value || '');
-  setIsEditing(false);
-};
 
-if (isEditing) {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: 4,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: 6,
-        }}
-      >
-        <input
-          type="date"
-          value={editedValue || ''}
-          aria-invalid={Boolean(dateError)}
-          onChange={event => {
-            setEditedValue(event.target.value);
-            setDateError('');
-          }}
-        />
-
-        <IconButton
-          hoverColor={BNPColors.ceruleanBlue}
-          type="button"
-          onClick={handleSave}
-        >
-          <Icon slug="checked-full" size={16} />
-        </IconButton>
-
-        <IconButton
-          hoverColor={BNPColors.ceruleanBlue}
-          type="button"
-          onClick={handleCancel}
-        >
-          <Icon slug="close" size={16} />
-        </IconButton>
-      </div>
-
-      {dateError && (
-        <span
-          role="alert"
-          style={{
-            color: 'red',
-            fontSize: 12,
-            whiteSpace: 'normal',
-            textAlign: 'center',
-          }}
-        >
-          {dateError}
-        </span>
-      )}
-    </div>
-  );
-}
