50066619ebb4100ddc338afda028b215ca6ecc30
Hassen Ben Amor <147496071+iiDaVoDkA@users.noreply.github.com>
2026-09-23 11:49:01 +0200

    Update hello2.js

--- hello2.js
+++ hello2.js
@@ -1,32 +1,9 @@
-const handleSave = () => {
-  const nbCommittees =
-    original?.initiativeGovernance?.committees?.length;
-
-  const selectedDate = new Date(editedValue);
-
-  if (nbCommittees === 2) {
-    const pre1 = original?.committeeDates?.[0];
-
-    if (pre1 && selectedDate <= new Date(pre1)) {
-      setIsInvalidDateModalOpen(true);
-      return;
-    }
-  }
-
-  if (nbCommittees === 3) {
-    const pre2 = original?.committeeDates?.[1];
-
-    if (pre2 && selectedDate <= new Date(pre2)) {
-      setIsInvalidDateModalOpen(true);
-      return;
-    }
-  }
-
-  updateInitiative(original.id, {
-    committeeDate: editedValue,
-    id: original.id,
-    updatedFromGlobalScreen: true,
-  });
-
-  setIsEditing(false);
-};
+updateInitiative(original.id, {
+  ...original,
+  committeeDate: editedValue,
+  committeeDates: original?.committeeDates?.map((date, index, dates) =>
+    index === dates.length - 1 ? editedValue : date
+  ),
+  id: original.id,
+  updatedFromGlobalScreen: true,
+});
